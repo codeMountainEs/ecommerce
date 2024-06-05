664416809be1f754c5fae50057179b0c2544bf1b
codeMountainEs <codemountain.es@icloud.com>
2024-06-05 16:59:25 +0200

    v7 add widget to Orders List

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListOrders extends ListRecords
@@ -16,4 +17,33 @@ class ListOrders extends ListRecords
             Actions\CreateAction::make(),
         ];
     }
+
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            OrderResource\Widgets\OrderStats::class,
+        ];
+    }
+    protected function getFooterWidgets(): array
+    {
+        return [
+            OrderResource\Widgets\OrderStats::class,
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            null => Tab::make('All'),
+            'new' => Tab::make()->query(fn($query) => $query->where('status','new')),
+            'processing' => Tab::make()->query(fn($query) => $query->where('status','processing')),
+            'shipped' => Tab::make()->query(fn($query) => $query->where('status','shipped')),
+            'delivered' => Tab::make()->query(fn($query) => $query->where('status','delivered')),
+            'cancelled' => Tab::make()->query(fn($query) => $query->where('status','cancelled')),
+
+
+        ];
+    }
+
 }
